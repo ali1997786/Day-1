8c299c6b8702a613a991829243b23696df7a20c4
Ishaque ali <ishaqueali7786@gmail.com>
2020-10-15 00:47:03 +0530

    first-day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Person from './Person/Person';
 import './App.css';
+ 
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons : [
+    {name:'Ali', age :28},
+    {name:'Shahid', age :28},
+    {name:'Ishaque', age :28}
+    ],
+    otherstate:'Something is there'
+  });
+  const switchNameHandler = () => {  
+    //console.log('Button Clicked!');
+    setPersonsState ({
+      persons:[
+        {name:' Ishaque Ali', age :28},
+        {name:'Shahid', age :28},
+        {name:'Ishaque', age :28}
+      ],
+    
+    });
+  };
+  
+     return (
+      <div className="App">
+         <h1>Hi I am a react app</h1>
+         <button onClick={switchNameHandler}>Switch Name</button>
+         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+         <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies  is Racing</Person>
+         <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+         
+       </div>
+    );
+ //return React.createElement('div',{className:'App'},React.createElement('hi', null, 'Hii I am react JS'));
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  
+  
 export default App;
+
+ 
+
